3b7f1bbd8b48d4c279d346e0d70da7321d1c8ec3
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-04 17:27:46 +0530

    CID-004 : Intro to Spring Profiles

--- src/main/java/com/paritosh/didemo/service/PrimaryService.java
+++ src/main/java/com/paritosh/didemo/service/PrimaryService.java
@@ -1,10 +1,12 @@
 package com.paritosh.didemo.service;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile("p2.english")
 public class PrimaryService implements GreetingService {
     @Override
     public String sayHello() {
