3a4b5e721f5990ee7f923bf2027baddff125a8e2
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-10 19:18:35 +0530

    CID-008 : Example to show how to read properties from multiple datasources/properties file.

--- src/main/java/com/paritosh/didemo/DiDemoApplication.java
+++ src/main/java/com/paritosh/didemo/DiDemoApplication.java
@@ -5,6 +5,7 @@ import com.paritosh.didemo.controller.MyController;
 import com.paritosh.didemo.controller.PropertyInjectedController;
 import com.paritosh.didemo.controller.SetterInjectedController;
 import com.paritosh.didemo.fakeBeans.FakeDataSource;
+import com.paritosh.didemo.fakeBeans.FakeJMSBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +23,9 @@ public class DiDemoApplication {
         //System.out.println(appCtxt.getBean(ConstructorInjectedController.class).sayHello());
         FakeDataSource fakeDataSource = (FakeDataSource) appCtxt.getBean(FakeDataSource.class);
         System.out.println("USERNAME: "+fakeDataSource.getDbUsername());
+
+        FakeJMSBroker fakeJMSBroker = (FakeJMSBroker) appCtxt.getBean(FakeJMSBroker.class);
+        System.out.println(fakeJMSBroker.getUsername());
 
     }
 }
--- src/main/java/com/paritosh/didemo/config/PropertConfig.java
+++ src/main/java/com/paritosh/didemo/config/PropertConfig.java
@@ -1,14 +1,20 @@
 package com.paritosh.didemo.config;
 
 import com.paritosh.didemo.fakeBeans.FakeDataSource;
+import com.paritosh.didemo.fakeBeans.FakeJMSBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties","classpath:jmsSpecific.properties"})
+@PropertySources({
+    @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jmsSpecific.properties")
+})
 public class PropertConfig {
     @Value("${com.paritosh.db.username}")
     String dbUsername;
@@ -16,6 +22,23 @@ public class PropertConfig {
     String dbPassword;
     @Value("${com.paritosh.db.dburl}")
     String dbUrl;
+
+    @Value("${com.paritosh.jms.username}")
+    String jmsUsername;
+    @Value("${com.paritosh.jms.password}")
+    String jmsPassword;
+    @Value("${com.paritosh.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeJMSBroker fakeJMSBroker(){
+        FakeJMSBroker fakeJMSBroker = new FakeJMSBroker();
+        fakeJMSBroker.setUsername(jmsUsername);
+        fakeJMSBroker.setPassword(jmsPassword);
+        fakeJMSBroker.setUrl(jmsUrl);
+        return fakeJMSBroker;
+    }
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
